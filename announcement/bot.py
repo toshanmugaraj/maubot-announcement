c5b4174d142bea1ac0913b0b43f54f4794c6ab4d
toshanmugaraj <toshanmugaraj@gmail.com>
2025-01-08 13:38:34 +0300

    update the ratelimit logic

--- announcement/bot.py
+++ announcement/bot.py
@@ -25,6 +25,7 @@ class Config(BaseProxyConfig):
 class Announcement(Plugin):
 
     async def process_queue(self):
+        normal_sleep_time = self.sleep_time 
         while True:
             try:
                 # Check for high-priority messages first
@@ -35,37 +36,50 @@ class Announcement(Plugin):
                     async with self.lock:
                         message = self.message_queue.popleft()  # Get regular message
                 else:
-                    await asyncio.sleep(self.sleep_time)  # Sleep briefly if both queues are empty
+                    await asyncio.sleep(normal_sleep_time)  # Sleep briefly if both queues are empty
                     self.log.debug(f"Waiting..................")
                     continue  # Go back to the start of the loop
 
                 # Send the message
-                if message.get('read_receipt'):
-                    await self.client.send_message(message['room_id'], message['content'])
-                else: 
-                    self.log.debug(f"Will send message ..................{message['room_id']}, {message['content']}")
-                    event_id = await self.client.send_message(message['room_id'], message['content'])
-                    if event_id:
-                        message_content = {
-                            "msgtype": "m.text",
-                            "body": f"🔔 *Sent for*:{message['user']}",
-                            "format": "org.matrix.custom.html",
-                            "formatted_body": f"🔔 <em>Sent for</em>: {message['user']}",
-                            "m.relates_to": {
-                                'rel_type': 'm.thread',
-                                'event_id': message['origin_evt_id']
+                try:
+                    if message.get('read_receipt'):
+                        await self.client.send_message(message['room_id'], message['content'])
+                    else: 
+                        self.log.debug(f"Will send message ..................{message['room_id']}, {message['content']}")
+                        event_id = await self.client.send_message(message['room_id'], message['content'])
+                        if event_id:
+                            message_content = {
+                                "msgtype": "m.text",
+                                "body": f"🔔 *Sent for*:{message['user']}",
+                                "format": "org.matrix.custom.html",
+                                "formatted_body": f"🔔 <em>Sent for</em>: {message['user']}",
+                                "m.relates_to": {
+                                    'rel_type': 'm.thread',
+                                    'event_id': message['origin_evt_id']
+                                }
                             }
-                        }
-                        message_read = {
-                            "room_id": message['origin_room_id'],
-                            "content": message_content,
-                            "read_receipt": True
-                        }
+                            message_read = {
+                                "room_id": message['origin_room_id'],
+                                "content": message_content,
+                                "read_receipt": True
+                            }
 
-                        async with self.lock:
-                            self.high_priority_queue.append(message_read)  # Always add high-priority messages
-
-                await asyncio.sleep(self.sleep_time)  # Wait based on the rate limit 
+                            async with self.lock:
+                                self.high_priority_queue.append(message_read)  # Always add high-priority messages
+                    self.sleep_time = normal_sleep_time
+                except Exception as e:
+                    error_message = str(e)
+                    if "Too Many Requests" in error_message:
+                        self.sleep_time *= 2 
+                        self.log.warning(f"Rate limit hit! Increasing sleep time {self.sleep_time}.")
+                        await asyncio.sleep(self.sleep_time)
+                        if message.get('read_receipt'):
+                            self.high_priority_queue.appendleft(message)
+                        else:
+                            self.message_queue.appendleft(message)
+                        continue
+                    else:
+                        self.log.error(f"Error sending message: {e}")
             except Exception as e:
                 self.log.error(f"Error processing message: {e}")
                 await asyncio.sleep(self.sleep_time)  # Optional: wait briefly before trying again
@@ -79,7 +93,7 @@ class Announcement(Plugin):
         self.message_queue = deque()
         self.high_priority_queue = deque()
         self.lock = asyncio.Lock()
-        self.rate_limit_per_second = 0.2 
+        self.rate_limit_per_second = 0.9 
         self.sleep_time = 1 / self.rate_limit_per_second
 
         # Start processing the queue as a background task
